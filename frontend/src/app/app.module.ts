f80d0bcb09e7670d1e8f384409b3cfc9fd03b5e1
Camilo Sanchez <camilosanchezdev@gmail.com>
2021-12-29 15:05:51 -0300

    feat - register & login components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './features/public/pages/login/login.component';
+import { RegisterComponent } from './features/public/pages/register/register.component'
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, LoginComponent, RegisterComponent],
     imports: [BrowserModule, AppRoutingModule, NgbModule],
     providers: [],
     bootstrap: [AppComponent],
