bb3a750a0ebdc47b9ed9e73f6b2af4a6429d7b64
Camilo Sanchez <camilosanchezdev@gmail.com>
2021-12-31 16:30:20 -0300

    feat - SigIn Form

--- frontend/src/app/features/public/pages/login/login.component.ts
+++ frontend/src/app/features/public/pages/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from 'src/app/core/services/auth.service'
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+    formGroup: FormGroup
+    constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+        this.formGroup = this.formBuilder.group({
+            username: [null, Validators.required],
+            password: [null, Validators.required],
+        })
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
+    onSubmit(formGroup: FormGroup): void {
+        if (formGroup.valid) {
+            this.authService
+                .signIn({
+                    username: formGroup.controls.username.value,
+                    password: formGroup.controls.password.value,
+                })
+                .subscribe()
+        }
+    }
 }
