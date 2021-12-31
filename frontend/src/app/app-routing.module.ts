dc39bb0e08fb80f2d1fc426d9d467c951c644289
Camilo Sanchez <camilosanchezdev@gmail.com>
2021-12-31 16:28:10 -0300

    feat - Add guards to routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { AuthUserGuard } from './common/guards/auth-user.guard'
+import { AuthGuard } from './common/guards/auth.guard'
 
 const routes: Routes = [
-    { path: '', loadChildren: () => import('./features/main/main.module').then((m) => m.MainModule) },
-    { path: 'user', loadChildren: () => import('./features/public/public.module').then((m) => m.PublicModule) },
+    { path: '', loadChildren: () => import('./features/main/main.module').then((m) => m.MainModule), canActivate: [AuthGuard] }, //, canActivate: [AuthGuard]
+    { path: 'user', loadChildren: () => import('./features/public/public.module').then((m) => m.PublicModule), canActivate: [AuthUserGuard] }, //, canActivate: [AuthUserGuard]
+    { path: '**', redirectTo: 'user' },
 ]
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/common/guards/auth-user.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core'
+import { CanActivate } from '@angular/router'
+import { Observable } from 'rxjs'
+import { AuthService } from 'src/app/core/services/auth.service'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthUserGuard implements CanActivate {
+    constructor(private authService: AuthService) {}
+    canActivate(): Observable<boolean> {
+        return this.authService.isAuthUser()
+    }
+}
