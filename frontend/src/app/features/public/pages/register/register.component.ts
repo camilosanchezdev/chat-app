fee7332f482952602eeaf73d4cbde1bf33a8f46d
Camilo Sanchez <camilosanchezdev@gmail.com>
2021-12-31 17:04:32 -0300

    feat - SignUp form

--- frontend/src/app/features/public/pages/register/register.component.ts
+++ frontend/src/app/features/public/pages/register/register.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { confirmPassValidator } from 'src/app/common/directives/confirm-pass.directive'
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+    formGroup: FormGroup
+    submitted: boolean = false
+    constructor(private formBuilder: FormBuilder) {
+        this.formGroup = this.formBuilder.group(
+            {
+                username: [null, Validators.required],
+                password: [null, Validators.required],
+                confirmPassword: [null, [Validators.required]],
+            },
+            { validators: confirmPassValidator }
+        )
+    }
 
-  constructor() { }
+    ngOnInit(): void {}
+    onSubmit(form: FormGroup): void {
+        this.submitted = true
+        console.log(form)
 
-  ngOnInit(): void {
-  }
-
+        if (form.valid) {
+            console.log({
+                username: form.controls.username.value,
+                password: form.controls.password.value,
+            })
+        }
+    }
 }
