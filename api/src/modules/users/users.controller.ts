f3e0e1a246e53b3742cc65d6ad6f5d2d8ce29bf7
Camilo Sanchez <camilosanchezdev@gmail.com>
2022-01-12 01:09:36 -0300

    feat: get online users

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, Post, Put, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
+import { ChangeAvatarDto } from './dto/change-avatar.dto'
 import { SignUpCredentialsDto } from './dto/signup-credentials.dto'
 import { GetUser } from './get-user.decorator'
 import { UserEntity } from './user.entity'
@@ -10,11 +11,6 @@ import { UsersService } from './users.service'
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
-    @Get()
-    @UseGuards(AuthGuard())
-    getAllOnline(@GetUser() user: UserEntity): Promise<any> {
-        return this.usersService.getAllOnline(user)
-    }
     @Post('/signup')
     signUp(@Body() signUpCredentialsDto: SignUpCredentialsDto): Promise<any> {
         return this.usersService.signUp(signUpCredentialsDto)
@@ -23,5 +19,16 @@ export class UsersController {
     @Post('/signin')
     signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<any> {
         return this.usersService.signIn(authCredentialsDto)
+    }
+    @Get()
+    @UseGuards(AuthGuard())
+    getAllOnline(@GetUser() user: UserEntity): Promise<any> {
+        return this.usersService.getAllOnline(user)
+    }
+
+    @Put('/avatar')
+    @UseGuards(AuthGuard())
+    changeAvatar(@GetUser() user: UserEntity, @Body() avatar: ChangeAvatarDto): Promise<any> {
+        return this.usersService.changeAvatar(user, avatar)
     }
 }
